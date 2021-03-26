38f0794c3e158dcc5f262c7b96d81910c8b329f5
DerrickArdron <42250783+DerrickArdron@users.noreply.github.com>
2021-03-26 21:46:30 +0000

    dataAdder modified to both Insert and Update as appropriate depending on whether there is an existing record

--- da_utils.py
+++ da_utils.py
@@ -21,17 +21,54 @@ def createTable(db, tableName, index, *cols):
     db.commit()
 
 
-def dataAdder(db,caller, table, **other):
-    keyList = list(other.keys())
-    valueList = list(other.values())
-    stmt = 'INSERT INTO ' + table + ' ' "(" + str(keyList)[1:-1].replace("'","") + ") VALUES ("+ str(valueList)[1:-1] + ")"
-    try:
-        db.query(stmt)
-        db.commit()
-    except Exception as e:
-        print(e)
+def valType(inPut):
+    if str(type(inPut)) == "<class 'int'>":
+        rtnVal = str(inPut)
+    else:
+        rtnVal = "'" + inPut + "'"
+    return rtnVal
+
+
+def dataAdder(db,caller, table, keyVal, **other):
+    '''
+    pKeyList = list(pKey.keys())
+    pKeyValues = list(pKey.values())
+
+    n = 0
+    pKeyStr =''
+    while n < len(pKey):
+        if n > 0:
+            pKeyStr = pKeyStr + ', '
+        pKeyStr = pKeyStr +pKeyList[n] +' = ' + valType(pKeyValues[n])
+        print('~39', pKeyStr)
+        n += 1
+    #pKeyStr = pKeyStr + ')'
+    '''
+
+    otherKeyList = list(other.keys())
+    otherValueList = list(other.values())
+    keyValueList = " (" + str(otherKeyList)[1:-1].replace("'","") + ") VALUES ("+ str(otherValueList)[1:-1] + ")"
+    n = 0
+    updateList = ''
+    while n < len(otherKeyList):
+        updateList = updateList + otherKeyList[n] + ' = ' + valType(otherValueList[n]) +", "
+        n+= 1
+    updateList = updateList[:-2]
+    print('~55 ', updateList)
+    stmt = "SELECT * from " + table +' where ' + keyVal
+    c = db.cursor()
+    c.execute(stmt)
+    itemString = str(c.fetchone())
+    if itemString.upper() == 'NONE':
+        stmt = 'INSERT INTO ' + table + keyValueList
+        try:
+            db.query(stmt)
+            db.commit()
+        except Exception as e:
+            print(e)
         pass
     else:
-        pass
-    finally:
-        pass
+        stmt = 'UPDATE ' + table + '\nSET '+ updateList +'\n WHERE ' + keyVal
+        print('~64 da_utils',stmt)
+        db.query(stmt)
+        db.commit()
